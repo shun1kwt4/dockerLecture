104e9ba07459c7886aa06daf9375efda28899a40
KawataShunichi <kawata.shunichi@mikilab.doshisha.ac.jp>
2024-06-02 22:13:06 +0900

    final_commit

--- backend/app/app.py
+++ backend/app/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import mysql.connector
 from mysql.connector import Error
-import bcrypt
+import bcrypt 
  
 app = Flask(__name__)
 CORS(app)
